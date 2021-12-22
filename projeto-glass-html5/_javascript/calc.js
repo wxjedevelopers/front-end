af9a08874166bfd3c7beee1d1e6413a1cce8f3ae
EmpireJEDevelopers <wkjorgesouza@gmail.com>
2021-12-22 20:42:18 -0300

    Projetos front-end

---
+++ projeto-glass-html5/_javascript/calc.js
@@ -0,0 +1,7 @@
+<script>
+    function calc_total() {
+        var qtd = parseInt(document.getElementById('cQtd').value);
+        tot = qtd * 1500;
+        document.getElementById('cTot').value = tot;
+    }
+</script>
