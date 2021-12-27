85104b6c5f020cb38ceb8158ea68b925c591f555
EmpireJEDevelopers <wkjorgesouza@gmail.com>
2021-12-27 17:29:29 -0300

    Primeira Atualização

---
+++ testejs-lampada/function.js
@@ -0,0 +1,12 @@
+function MudaLampada(tipo) {
+    if (tipo == 1) {
+        arquivo = "http://i.stack.imgur.com/ybxlO.jpg";
+    }
+    if (tipo == 2) {
+        arquivo = "http://i.stack.imgur.com/ybxlO.jpg";
+    }
+    if (tipo == 3) {
+        arquivo = "http://i.stack.imgur.com/MRjsF.jpg";
+    }
+    document.getElementById("luz").src = arquivo;
+}
